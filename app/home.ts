aafe9c86c09f73a96a2725bd035f7deedb8557ff
Dmitry Vasilyev <vaseker@yandex-team.ru>
2016-02-10 02:38:35 +0500

    Added simple page

---
+++ app/home.ts
@@ -0,0 +1,8 @@
+import { Component } from 'angular2/core';
+
+@Component({
+  selector: 'page-home',
+  template: `<h3>Homepage</h3><p>Empty page without ag-grid</p>`
+})
+
+export class Home {}
--- app/index.ts
+++ app/index.ts
@@ -1,13 +1,15 @@
 import { Component } from 'angular2/core';
 import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from 'angular2/router';
 
-import { AppComponent } from './app.component'
+import { AppComponent } from './app.component';
+import { Home } from './home';
 
 @Component({
   selector: 'app',
   directives: [ROUTER_DIRECTIVES],
   providers: [ROUTER_PROVIDERS],
   template: `<nav>
+    <a [routerLink]="['Home']">Home</a>
     <span>Table type: </span>
     <a [routerLink]="['Table', { type: 'stock' }]">Stock</a>
     <a [routerLink]="['Table', { type: 'oil' }]">Oil</a>
@@ -16,6 +18,7 @@ import { AppComponent } from './app.component'
 })
 
 @RouteConfig([
+  { path: '/', name: 'Home',  component: Home, useAsDefault: true },
   { path: '/table/:type', name: 'Table',  component: AppComponent }
 ])
 
